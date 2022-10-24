306bd9313dbec33e61825e4df8d23081cb92bd98
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-10-24 13:18:58 -0300

    Got the ShowHud and PickUp working

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -8,8 +8,44 @@ namespace Lists
 {
     internal class Program
     {
+        static List<string> Inventory = new List<string>();
+        static int maxInventory = 8;
+
         static void Main(string[] args)
         {
+            ShowHud();
+            PickUp("Healing Potion");
+            ShowHud();
         }
+
+        static void ShowHud()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Inventory");
+            Console.WriteLine(Inventory.Count + " items");
+            foreach (string item in Inventory)
+            {
+                Console.WriteLine("|" + item + "|");
+            }
+            Console.ResetColor();
+            Console.ReadKey(true);
+        }
+
+        static void PickUp(string item)
+        {
+            Console.WriteLine("Trying to add " + item);
+            if(Inventory.Count < maxInventory)
+            {
+                Inventory.Add(item);
+                Console.WriteLine("Item Added");
+            }
+            else
+            {
+                Console.WriteLine("No room for the " + item);
+            }
+        }
+
+        
+
     }
 }
